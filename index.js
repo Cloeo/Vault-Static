4aa4ad3deb91d7b4d9f7ebe540713c23be0e52fc
Cloeo <118498952+Cloeo@users.noreply.github.com>
2026-03-22 13:59:57 -0500

    Refactor static file serving and path handling

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('.'));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/accountauth', (req, res) => {
-  res.sendFile(__dirname + '/accountauth.html');
+  res.sendFile(path.join(__dirname, 'accountauth.html'));
 });
 
 app.listen(process.env.PORT || 3000);
