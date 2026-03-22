0d9ad7c31f52ecbf6640af2c199254b80eea24c6
Cloeo <118498952+Cloeo@users.noreply.github.com>
2026-03-22 13:53:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const app = express();
 
+app.use(express.static('.'));
+
+app.get('/accountauth', (req, res) => {
+  res.sendFile(__dirname + '/accountauth.html');
+});
+
+app.listen(process.env.PORT || 3000);
